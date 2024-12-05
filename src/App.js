a29cc1f30e1edd1ec4ef9189aeaf3503c1cf2e1d
Masterhazi <hajiafridbaba@gmail.com>
2024-12-06 04:08:08 +0530

    useless

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import Analytics from '..components/Analytics';
+import Analytics from './Analytics';
 
 function App() {
   const [tasks, setTasks] = useState([]);
